9201ed0e5efb3280e1796ef873ee313aef1ca694
Johan Öhman <johan.ohman@ltu.se>
2019-11-07 14:56:45 +0100

    Added printing. Defulted to off

--- projected_area_ellipsoid.m
+++ projected_area_ellipsoid.m
@@ -16,7 +16,7 @@ function [Ax,Ay,Az] = projected_area_ellipsoid(Lx,Ly,Lz,phi,theta)
 %   Ay:     Projected area in y direction
 %   Az: 	Projected area in z direction
 %------------------------------------------------
-
+verbose = 0
 %Create the eigenvectors from the input angles
 v1 = [cos(phi)*cos(theta),sin(phi)*cos(theta),-sin(theta)]'; %"rotated x-axis"
 v2 = [-sin(phi),cos(phi),0]';    %"rotated y-axis"
@@ -39,6 +39,19 @@ Ax = Calc_area(Px);
 Ay = Calc_area(Py);
 Az = Calc_area(Pz);
 
+
+
+%
+if verbose = 1
+    isosurface(X, Y, Z, A(1,1)*X.^2 + 2*A(1,2)*X.*Y + A(2,2)*Y.^2 +...
+        A(3,3)*Z.^2 + 2*A(1,3)*X.*Z+ 2*A(2,3)*Y.*Z, r^2);
+    camlight(60,0);shading flat;view([30,15]);
+    S=A(1:2,1:2)-(1/A(3,3))*A(1:2,3)*A(3,1:2);%Schur complement
+    u=S(1,1);v=S(2,2);w=S(2,1);
+    ff = @(x,y) (u*x.^2 + v*y.^2 + 2*w*x.*y-r^2);% please note the -1
+    ep=ezplot(ff);set(ep,'linecolor',[0.5,0.5,0.5],'linewidth',3);
+end
+
 end
 function Area = Calc_area(P)
 % The eigenvalues of P containes information about the major/minor axis
@@ -55,11 +68,11 @@ if dir == 'x'
     
 elseif dir == 'y'
     P = [A(1,1)-A(1,2)^2/A(2,2), A(1,3)-A(2,3)*A(1,2)/A(2,2); ...
-         A(1,3)-A(2,3)*A(1,2)/A(2,2), A(3,3) - A(2,3)^2/A(3,3)];
+        A(1,3)-A(2,3)*A(1,2)/A(2,2), A(3,3) - A(2,3)^2/A(3,3)];
     
 elseif dir == 'z'
     P = [A(1,1)-A(1,3)^2/A(3,3),A(1,2)-A(1,3)*A(2,3)/A(3,3); ...
-         A(1,2)-A(1,3)*A(2,3)/A(3,3), A(2,2)-A(2,3)^2/A(3,3)];
+        A(1,2)-A(1,3)*A(2,3)/A(3,3), A(2,2)-A(2,3)^2/A(3,3)];
     
 else
     error('Invalid Direction')
